2820f7f3fcbe7157e864728e512544fc8eae37ac
Zalvie <Zalvie@users.noreply.github.com>
2014-10-10 05:56:14 +0200

    Create smaller.py
    Cuz why the fuck not

---
+++ cf/python/smaller.py
@@ -0,0 +1,25 @@
+
+cat = ['+[]', '+!![]'] + [''.join(['!+[]'] + ['+!![]' for _ in (range(1, i))]) for i in range(2, 10)]
+s = '!+[]+!![]'
+
+def calc(s):
+  output = None
+
+  for o in ''.join(['\n' if e in list('()') else e for e in list(s)]).split('\n'):
+    if '[' not in o: continue
+
+    answer = 1 if o.replace('[]+[]', '[]') == '!![]' else cat.index(o.replace('[]+[]', '[]'))
+
+    if isinstance(output, basestring) or '[]+[]' in o:
+        answer = str(answer)
+    
+    if output == None:
+      output = answer 
+    else: 
+      output += answer
+
+  return output
+
+
+for l in ['+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]))','+((!+[]+!![]+[])+(+!![]))','+((+!![]+[])+(!+[]+!![]+!![]+!![]))','+((!+[]+!![]+[])+(!+[]+!![]+!![]))','!+[]+!![]']:
+    print calc(l)

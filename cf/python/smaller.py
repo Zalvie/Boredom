3d14405803a495fb6af1ac52070a90d6c70522b4
Zalvie <Zalvie@users.noreply.github.com>
2014-10-10 06:14:42 +0200

    Update smaller.py

--- cf/python/smaller.py
+++ cf/python/smaller.py
@@ -1,5 +1,19 @@
+import operator as op; import re, sys
 
 cat = ['+[]', '+!![]'] + [''.join(['!+[]'] + ['+!![]' for _ in (range(1, i))]) for i in range(2, 10)]
+
+parse = '''
+       var t,r,a,f, GgydmCQ={"FblpsuEanzgG":+((!+[]+!![]+!![]+[])+(+[]))};
+        t = document.createElement('div');
+        t.innerHTML="<a href='/'>x</a>";
+        t = t.firstChild.href;r = t.match(/https?:\/\//)[0];
+        t = t.substr(r.length); t = t.substr(0,t.length-1);
+        a = document.getElementById('jschl-answer');
+        f = document.getElementById('challenge-form');
+        ;GgydmCQ.FblpsuEanzgG+=!+[]+!![]+!![]+!![]+!![];GgydmCQ.FblpsuEanzgG-=!+[]+!![]+!![]+!![]+!![];GgydmCQ.FblpsuEanzgG+=!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![];GgydmCQ.FblpsuEanzgG*=+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]));GgydmCQ.FblpsuEanzgG-=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]));GgydmCQ.FblpsuEanzgG-=+((!+[]+!![]+!![]+[])+(+[]));GgydmCQ.FblpsuEanzgG-=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]));GgydmCQ.FblpsuEanzgG*=+((!+[]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));GgydmCQ.FblpsuEanzgG+=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));
+'''
+
+items = [filter(None, x) for x in re.findall(r'(?:(?:"(:)(.*?)})|([-+\*\/])=(.*?))\;', parse)]
 
 def calc(s):
   output = None
@@ -19,6 +33,11 @@ def calc(s):
 
   return output
 
+def calculate(y, z, output):
+  return ({'+': op.add, '-': op.sub, '*': op.mul, '/': op.div}[z])(output, y)
 
-for l in ['+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]))','+((!+[]+!![]+[])+(+!![]))','+((+!![]+[])+(!+[]+!![]+!![]+!![]))','+((!+[]+!![]+[])+(!+[]+!![]+!![]))','!+[]+!![]']:
-    print calc(l)
+for s, l in items:
+    c = int(calc(l))
+    output = c if s == ':' else calculate(c, s, output)
+
+print output
